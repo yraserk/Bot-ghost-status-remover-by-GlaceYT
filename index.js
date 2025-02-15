15e3fae6cce76790cf9bd43e2f4f3f434a5a6a89
yraserk <yraserk@gmail.com>
2025-02-15 12:24:04 +0100

    index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["🎧 Listening to Feedback", "🎮 Playing Blender"];
+const statusTypes = [ 'online', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
